947b3d5f9ada1cbb97cfd9aa3cbd6486ddcce5c6
yangxiujie <xiujie_yang@yahoo.com>
2012-05-30 20:57:50 -0400

    changes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,12 @@
 class PagesController < ApplicationController
   def home
+    @title="home"
   end
 
   def contact
+  end
+
+  def about
+    @title="home"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ App::Application.routes.draw do
 
   get "pages/contact"
 
+  get "pages/about"
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
